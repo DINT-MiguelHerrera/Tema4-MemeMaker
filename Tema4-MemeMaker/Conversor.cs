2485eb986a77e2962fa9cb817f703944caca628e
alumno <alumno@A302PC35>
2023-11-09 09:56:05 +0100

    MemeMaker Acabado

---
+++ Tema4-MemeMaker/Conversor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+
+namespace Tema4_MemeMaker
+{
+    internal class Conversor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool)
+            {
+                if ((bool)value)
+                {
+                    return new Thickness(3, 3, 3, 3);
+                }
+                else
+                {
+                    return new Thickness(0, 0, 0, 0);
+                }
+            }
+            return new Thickness(0, 0, 0, 0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
